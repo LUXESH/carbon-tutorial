6ffab02fa7b93713f0fbe3271ff326bad50d12c6
Luz Segura <lucykatpendragon@gmail.com>
2023-05-23 22:36:36 -0600

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RepoTable from './RepoTable';
-import { Grid, Column } from '@carbon/react';
+import {
+  Link,
+  DataTableSkeleton,
+  Pagination,
+  Grid,
+  Column,
+} from '@carbon/react';
+import { Octokit } from '@octokit/core';
 
 const headers = [
   {
@@ -28,7 +35,7 @@ const headers = [
     header: 'Links',
   },
 ];
-
+/*
 const rows = [
   {
     id: '1',
@@ -58,15 +65,103 @@ const rows = [
     links: 'Links',
   },
 ];
+*/
+
+const octokitClient = new Octokit({});
 
 const RepoPage = () => {
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function getCarbonRepos() {
+      const res = await octokitClient.request('GET /orgs/{org}/repos', {
+        org: 'carbon-design-system',
+        per_page: 75,
+        sort: 'updated',
+        direction: 'desc',
+      });
+
+      if (res.status === 200) {
+        setRows(getRowItems(res.data));
+      } else {
+        setError('Error obtaining repository data');
+      }
+      setLoading(false);
+    }
+
+    getCarbonRepos();
+  }, []);
+
+  if (loading) {
+    return (
+      <Grid className="repo-page">
+        <Column lg={16} md={8} sm={4} className="repo-page__r1">
+          <DataTableSkeleton
+            columnCount={headers.lenght + 1}
+            rowCount={10}
+            headers={headers}
+          />
+          <Pagination
+            totalItems={rows.length}
+            backwardText="Previus page"
+            forwardText="Next page"
+            pageSize={currentPageSize}
+            pageSizes={[5, 10, 15, 25]}
+            itemsPerPageText="Items per page"
+            onChange={({ page, pageSize }) => {
+              if (pageSize !== currentPageSize) {
+                setCurrentPageSize(pageSize);
+              }
+              setFirstRowIndex(pageSize * (page - 1));
+            }}
+          />
+        </Column>
+      </Grid>
+    );
+  }
+  if (error) {
+    return `Error! ${error}`;
+  }
+
   return (
     <Grid className="repo-page">
       <Column lg={16} className="repo-page__r1">
-        <RepoTable headers={headers} rows={rows} />
+        <RepoTable
+          headers={headers}
+          rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+        />
       </Column>
     </Grid>
   );
 };
+
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homapage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const getRowItems = rows =>
+  rows.map(row => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers_count,
+    issueCount: row.open_issues_count,
+    createdAt: new Date(row.created_at).toLocaleDateString(),
+    updateAt: new Date(row.updated_at).toLocaleDateString(),
+    links: <LinkList url={row.html_url} homepageUrl={row.page} />,
+  }));
 
 export default RepoPage;
